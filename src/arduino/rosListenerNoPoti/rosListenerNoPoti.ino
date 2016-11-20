33817d604bedf04af96b91eedbc37a58c2845639
Cognirobo <leininger.robotik@googlemail.com>
2016-11-20 21:04:22 +0100

    softstarte function

--- src/arduino/rosListenerNoPoti/rosListenerNoPoti.ino
+++ src/arduino/rosListenerNoPoti/rosListenerNoPoti.ino
@@ -14,83 +14,89 @@ const int pwmPin6 = 6; //motor2 PWM
 const int pwmPin7 = 7; //motor2 B1
 const int pmwPin8 = 8; //motor2 B2
 
-int PotiVal=0;
+int motorSpeed=40;
 int PWM=0;
 int num=0;
+int wasActivated = 0;
+
 
 
 ros::NodeHandle  nh;
 
 void messageCb( const std_msgs::String& input){
   // Start system
-  digitalWrite(12, HIGH);      //YELLOW LED ROS message callback online
   if(strlen( (const char* ) input.data) == 1) {
-  digitalWrite(13, LOW);
-  digitalWrite(2, LOW);
-  digitalWrite(3, LOW);
-  digitalWrite(7, HIGH);
-  digitalWrite(8, LOW);
-  analogWrite(5, 0);
-  analogWrite(6, 0);
-  
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(7, LOW);
+    digitalWrite(8, LOW);
+    analogWrite(5, 0);
+    analogWrite(6, 0);
+    motorSpeed = 0;
   }
   // Go Forward
   if(strlen( (const char* ) input.data) == 2) {
-  digitalWrite(13, HIGH);    //GREEN LED ROS message active command
-  digitalWrite(2, HIGH);
-  digitalWrite(3, LOW);
-  digitalWrite(7, HIGH);
-  digitalWrite(8, LOW);
-  PotiVal = 120;
-  PWM= DutyCycle(PotiVal);
-  analogWrite(5, PWM);
-  analogWrite(6, PWM);
+    digitalWrite(13, HIGH);    //GREEN LED ROS message active command
+    digitalWrite(2, HIGH);
+    digitalWrite(3, LOW);
+    digitalWrite(7, HIGH);
+    digitalWrite(8, LOW);
+    motorSpeed = SoftStart(motorSpeed);
+    PWM= DutyCycle(motorSpeed);
+    motorSpeed = motorSpeed;
+    analogWrite(5, PWM);
+    analogWrite(6, PWM);
   }
   //Go backward
   if(strlen( (const char* ) input.data) == 3) {
-  digitalWrite(13, HIGH);
-  digitalWrite(2, LOW);
-  digitalWrite(3, HIGH);
-  digitalWrite(7, LOW);
-  digitalWrite(8, HIGH);
-  PotiVal = 120;
-  PWM= DutyCycle(PotiVal);
-  analogWrite(5, PWM);
-  analogWrite(6, PWM);
+    digitalWrite(13, HIGH);
+    digitalWrite(2, LOW);
+    digitalWrite(3, HIGH);
+    digitalWrite(7, LOW);
+    digitalWrite(8, HIGH);
+    motorSpeed = SoftStart(motorSpeed);
+    PWM= DutyCycle(motorSpeed);
+    motorSpeed = motorSpeed;
+    analogWrite(5, PWM);
+    analogWrite(6, PWM);
   }
   //Turn Left
   if(strlen( (const char* ) input.data) == 4) {
-  digitalWrite(13, HIGH);
-  digitalWrite(2, HIGH);
-  digitalWrite(3, LOW);
-  digitalWrite(7, LOW);
-  digitalWrite(8, HIGH);
-  PotiVal = 120;
-  PWM= DutyCycle(PotiVal);
-  analogWrite(5, PWM);
-  analogWrite(6, PWM);
+    digitalWrite(13, HIGH);
+    digitalWrite(2, LOW);
+    digitalWrite(3, HIGH);
+    digitalWrite(7, HIGH);
+    digitalWrite(8, LOW);
+    motorSpeed = SoftStart(motorSpeed);
+    PWM= DutyCycle(motorSpeed);
+    motorSpeed = motorSpeed;
+    analogWrite(5, PWM);
+    analogWrite(6, PWM);
   }
   //Turn Right
   if(strlen( (const char* ) input.data) == 5) {
-  digitalWrite(13, HIGH);
-  digitalWrite(2, LOW);
-  digitalWrite(3, HIGH);
-  digitalWrite(7, HIGH);
-  digitalWrite(8, LOW);
-  PotiVal = 120;
-  PWM= DutyCycle(PotiVal);
-  analogWrite(5, PWM);
-  analogWrite(6, PWM);
+    digitalWrite(13, HIGH);
+    digitalWrite(2, HIGH);
+    digitalWrite(3, LOW);
+    digitalWrite(7, LOW);
+    digitalWrite(8, HIGH);
+    motorSpeed = SoftStart(motorSpeed);
+    PWM= DutyCycle(motorSpeed);
+    motorSpeed = motorSpeed;
+    analogWrite(5, PWM);
+    analogWrite(6, PWM);
   }
   //Stop
   if(strlen( (const char* ) input.data) == 6) {
-  digitalWrite(13, LOW);
-  digitalWrite(2, LOW);
-  digitalWrite(3, LOW);
-  digitalWrite(7, LOW);
-  digitalWrite(8, LOW);
-  analogWrite(5, 0);
-  analogWrite(6, 0);
+    digitalWrite(10, HIGH);      //YELLOW LED ROS message callback online
+    digitalWrite(13, LOW);
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(7, LOW);
+    digitalWrite(8, LOW);
+    analogWrite(5, 0);
+    analogWrite(6, 0);
+    motorSpeed = 0;
   }
 }
 
@@ -102,6 +108,7 @@ void setup()
   pinMode(3, OUTPUT);
   pinMode(7, OUTPUT);
   pinMode(8, OUTPUT);
+  pinMode(10, OUTPUT);
   pinMode(11, OUTPUT);
   pinMode(12, OUTPUT);
   pinMode(13, OUTPUT);
@@ -116,14 +123,23 @@ void loop()
   delay(0.1);
 }
 
-int DutyCycle(int Val){
+int DutyCycle(int motorSpeed){
   const int Untergrenze=40; //20% Duty Cycle
-  const int Obergrenze=140;  //50% Duty Cycle
-  
   int backValue=0;
-  
-  backValue = Untergrenze+(0.098*Val);
-  //backValue=Val/4;
-  
+  backValue = Untergrenze +(0.098*motorSpeed);  
   return backValue;
 }
+
+int SoftStart(int motorSpeed){
+  if (motorSpeed < 100) {
+    motorSpeed = 400;
+    return motorSpeed;
+  }
+  if (motorSpeed >= 1400) { // 2100 ist the max 245 for duty cycle to have only 50% max is 880
+    motorSpeed = 1400;
+  return motorSpeed;
+  }
+  motorSpeed = motorSpeed + 5;
+  return motorSpeed;
+}
+
